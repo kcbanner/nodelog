722a30f8d06201831da37380c037b348fb0ec517
Casey Banner <kcbanner@gmail.com>
2011-02-23 11:06:34 -0500

    * Style work

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var settings = require('./settings');
 var models = require('./models');
+var date = require('datejs');
 
 var app = module.exports = express.createServer();
 
@@ -35,11 +36,11 @@ app.get('/', function(req, res){
   var posts = [{title: 'This is the test title',
                permalink: 'test-link-1',
                content: 'This is a really long post, not much here just a bunch of words bla bla bla',
-               date: new Date()},
+               date: new Date().toString('MMMM dS')},
                {title: 'This is the test title #2',
                permalink: 'test-link-2',
                content: 'This is a really long post, not much here just a bunch of words bla bla bla',
-               date: new Date()}];
+               date: new Date().toString('MMMM dS')}];
   
   res.render('index', {
     locals: {

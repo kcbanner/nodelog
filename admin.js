873d7307985e8ad137c4c000bfcd46c1cf973c1f
Casey Banner <kcbanner@gmail.com>
2011-03-16 12:40:08 -0400

    * Seperated admin view functions into seperate file.

---
+++ admin.js
@@ -0,0 +1,150 @@
+var settings = require('./settings');
+var models = require('./models');
+
+exports.require_login = function (req, res, next) {
+  if(req.session.user) {
+    next();
+  } else {
+    res.redirect('/admin/login');
+  };
+};
+
+exports.logout = function(req, res) {
+  req.session.regenerate(function(err, destroyedBoolean) {
+    res.redirect('/admin/login');
+  });
+};
+
+exports.login = function(req, res) {
+  res.local('error', false);
+  res.render('admin_login');
+};
+
+exports.login_post = function(req, res) {
+  var username = req.param('username');
+  var password = req.param('password');
+  if(username !== undefined && password !== undefined) {
+    var hashed_passwd = crypto.createHash('sha1').update(settings.secret+req.param('password')).digest('hex');
+    if(username == settings.admin.username && hashed_passwd == settings.admin.password) {
+      req.session.user = true;
+      res.redirect('/admin');
+      return;
+    }
+  };
+
+  res.local('error', true);
+  res.render('admin_login');
+};
+
+exports.index = function(req, res) {
+  res.render('admin');
+};
+
+exports.ad = function(req, res) {
+  res.local('ad', res.local('ad') || {});
+  res.render('admin_ad');
+};
+
+exports.ad_post = function(req, res) {
+  var ad;
+
+  var save_ad = function() {
+    ad.title = req.param('title');
+    ad.code = req.param('code');
+    ad.save(function(err) {
+      res.redirect('/admin/ad');    
+    });    
+  };
+
+  if (req.param('id')) {
+    var q = models.Ad.find({_id: req.param('id')}).limit(1);
+    q.execFind(function(err, ads) {
+      if (ads.length == 1) {
+        ad = ads[0];
+        save_ad();
+      } else {
+        throw new Error('Ad not found.');
+      }
+    });
+  } else {
+    ad = new models.Ad();
+    ad.active = true;
+    save_ad();
+  }
+};
+
+exports.post_index = function(req, res) {
+  var q = models.Post.find({}).sort('date', -1);
+  q.execFind(function(err, posts) {
+    res.local('posts', posts);
+    res.render('admin_posts');
+  });
+};
+
+exports.post_new = function(req, res, next) {
+  res.local('post', {});
+  return res.render('admin_edit');
+};
+
+exports.post_new_save = function(req, res, next) {
+  var post = new models.Post();
+  post.author = settings.admin.username;
+  post.title = req.param('title');
+  post.permalink = req.param('permalink');
+  post.content = req.param('content');
+  post.date = new Date();
+  post.published = true;
+
+  if (post.title && post.permalink && post.content) {
+    post.save(function(err) {
+      res.redirect('/admin/post/edit/'+post.id);
+    });
+  } else {
+    res.local('post', post);
+    return res.render('admin_edit');
+  }
+};
+
+exports.post_edit = function(req, res, next) {
+  var q = models.Post.find({_id: req.params.id});
+  q.execFind(function(err, posts) {
+    if(posts) {
+      res.local('post', posts[0]);
+    } else {
+      return next(new NotFound);
+    }
+
+    return res.render('admin_edit');
+  });
+};
+
+exports.post_edit_save = function(req, res) {
+  var q = models.Post.find({_id: req.params.id});
+  q.execFind(function(err, posts) {
+    if(posts.length == 1) {
+      var post = posts[0];
+      post.title = req.param('title');
+      post.permalink = req.param('permalink');
+      post.content = req.param('content');
+      
+      post.save(function(err) {
+        res.redirect('/admin/post/edit/'+post.id);
+      });
+    } else {
+      return next(new NotFound);
+    }
+  });
+};
+
+exports.post_delete = function(req, res) {
+  var q = models.Post.find({_id: req.params.id});
+  q.execFind(function(err, posts) {
+    if(posts.length == 1) {
+      posts[0].remove(function(err) {
+        res.redirect('/admin/post');
+      });
+    } else {
+      return next(new NotFound);
+    }
+  });
+};
--- app.js
+++ app.js
@@ -2,6 +2,7 @@
  * NodeLog
  */
 
+// Libs
 var sys = require('sys');
 var express = require('express');
 var mongoose = require('mongoose');
@@ -9,9 +10,10 @@ var crypto = require('crypto');
 var MongoStore = require('connect-mongo');
 
 var settings = require('./settings');
-var util = require('./util');
 var db = mongoose.connect('mongodb://localhost/'+settings.db);
+var util = require('./util');
 var models = require('./models');
+var admin = require('./admin');
 
 var app = module.exports = express.createServer();
 
@@ -82,7 +84,7 @@ function get_ad(req, res, next) {
   });
 }
 
-var stack = [locals];
+app.use(locals);
 
 // Error handling
 function NotFound(msg){
@@ -102,7 +104,7 @@ app.error(function(err, req, res, next) {
 });
 
 // Routes
-app.get('/feed.rss', stack, function(req, res) {
+app.get('/feed.rss', function(req, res) {
   var q = models.Post.find({published: true}).sort('date', -1).limit(20);
   q.execFind(function(err, posts) {
     var post = posts[0];
@@ -112,7 +114,7 @@ app.get('/feed.rss', stack, function(req, res) {
   });
 });
 
-app.get(/^\/(?:page\/(\d+))?$/, stack, get_ad, function(req, res) {
+app.get(/^\/(?:page\/(\d+))?$/, get_ad, function(req, res) {
   var q = models.Post.find({published: true}).sort('date', -1).limit(settings.front_page_posts);
   var page = 0;
   if (req.params[0] !== undefined) {
@@ -128,7 +130,7 @@ app.get(/^\/(?:page\/(\d+))?$/, stack, get_ad, function(req, res) {
   });
 });
 
-app.get(/^\/(\d{4})\/(\d{2})\/(\d{2})\/([a-zA-Z-0-9]+)\/?/, stack, get_ad, function(req, res, next){
+app.get(/^\/(\d{4})\/(\d{2})\/(\d{2})\/([a-zA-Z-0-9]+)\/?/, get_ad, function(req, res, next){
   var q = models.Post.findOne({published: true, permalink: req.params[3]});
   q.execFind(function(err, posts) {
     if(posts.length == 0) {
@@ -143,158 +145,21 @@ app.get(/^\/(\d{4})\/(\d{2})\/(\d{2})\/([a-zA-Z-0-9]+)\/?/, stack, get_ad, funct
 });
 
 /* Admin */
-function require_login(req, res, next) {
-  if(req.session.user) {
-    next();
-  } else {
-    res.redirect('/admin/login');
-  };
-};
-
-app.all('/admin/logout', function(req, res) {
-  req.session.regenerate(function(err, destroyedBoolean) {
-    res.redirect('/admin/login');
-  });
-});
-
-app.get('/admin/login', stack, function(req, res) {
-  res.local('error', false);
-  res.render('admin_login');
-});
-
-app.post('/admin/login', stack, function(req, res) {
-  var username = req.param('username');
-  var password = req.param('password');
-  if(username !== undefined && password !== undefined) {
-    var hashed_passwd = crypto.createHash('sha1').update(settings.secret+req.param('password')).digest('hex');
-    if(username == settings.admin.username && hashed_passwd == settings.admin.password) {
-      req.session.user = true;
-      res.redirect('/admin');
-      return;
-    }
-  };
-
-  res.local('error', true);
-  res.render('admin_login');
-});
-
-/* Admin Editing */
-
-app.get('/admin', stack, require_login, function(req, res) {
-  res.render('admin');
-});
-
-app.get('/admin/ad', stack, get_ad, require_login, function(req, res) {
-  res.local('ad', res.local('ad') || {});
-  res.render('admin_ad');
-});
-
-app.post('/admin/ad', stack, require_login, function(req, res) {
-  var ad;
-
-  var save_ad = function() {
-    ad.title = req.param('title');
-    ad.code = req.param('code');
-    ad.save(function(err) {
-      res.redirect('/admin/ad');    
-    });    
-  };
-
-  if (req.param('id')) {
-    var q = models.Ad.find({_id: req.param('id')}).limit(1);
-    q.execFind(function(err, ads) {
-      if (ads.length == 1) {
-        ad = ads[0];
-        save_ad();
-      } else {
-        throw new Error('Ad not found.');
-      }
-    });
-  } else {
-    ad = new models.Ad();
-    ad.active = true;
-    save_ad();
-  }
-});
-
-app.get('/admin/post', stack, require_login, function(req, res) {
-  var q = models.Post.find({}).sort('date', -1);
-  q.execFind(function(err, posts) {
-    res.local('posts', posts);
-    res.render('admin_posts');
-  });
-});
-
-app.get('/admin/post/new', stack, require_login, function(req, res, next) {
-  res.local('post', {});
-  return res.render('admin_edit');
-});
-
-app.post('/admin/post/new', stack, require_login, function(req, res, next) {
-  var post = new models.Post();
-  post.author = settings.admin.username;
-  post.title = req.param('title');
-  post.permalink = req.param('permalink');
-  post.content = req.param('content');
-  post.date = new Date();
-  post.published = true;
-
-  if (post.title && post.permalink && post.content) {
-    post.save(function(err) {
-      res.redirect('/admin/post/edit/'+post.id);
-    });
-  } else {
-    res.local('post', post);
-    return res.render('admin_edit');
-  }
-});
-
-app.get('/admin/post/edit/:id', stack, require_login, function(req, res, next) {
-  var q = models.Post.find({_id: req.params.id});
-  q.execFind(function(err, posts) {
-    if(posts) {
-      res.local('post', posts[0]);
-    } else {
-      return next(new NotFound);
-    }
-
-    return res.render('admin_edit');
-  });
-});
-
-app.post('/admin/post/edit/:id', stack, require_login, function(req, res) {
-  var q = models.Post.find({_id: req.params.id});
-  q.execFind(function(err, posts) {
-    if(posts.length == 1) {
-      var post = posts[0];
-      post.title = req.param('title');
-      post.permalink = req.param('permalink');
-      post.content = req.param('content');
-      
-      post.save(function(err) {
-        res.redirect('/admin/post/edit/'+post.id);
-      });
-    } else {
-      return next(new NotFound);
-    }
-  });
-});
-
-app.get('/admin/post/delete/:id', stack, require_login, function(req, res) {
-  var q = models.Post.find({_id: req.params.id});
-  q.execFind(function(err, posts) {
-    if(posts.length == 1) {
-      posts[0].remove(function(err) {
-        res.redirect('/admin');
-      });
-    } else {
-      return next(new NotFound);
-    }
-  });
-});
+app.all('/admin/logout', admin.logout);
+app.get('/admin/login', admin.login);
+app.post('/admin/login', admin.login_post);
+app.get('/admin', admin.require_login, admin.index);
+app.get('/admin/ad', admin.require_login, get_ad, admin.ad);
+app.post('/admin/ad', admin.require_login, admin.ad_post);
+app.get('/admin/post', admin.require_login, admin.post_index);
+app.get('/admin/post/new', admin.require_login, admin.post_new);
+app.post('/admin/post/new', admin.require_login, admin.post_new_save);
+app.get('/admin/post/edit/:id', admin.require_login, admin.post_edit);
+app.post('/admin/post/edit/:id', admin.require_login, admin.post_edit_save);
+app.get('/admin/post/delete/:id', admin.require_login, admin.post_delete);
 
 // 404
-app.all('*', stack, function(req, res) {
+app.all('*', function(req, res) {
   throw new NotFound;
 });
 
